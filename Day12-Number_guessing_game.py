7047e87ef4acd2b5c6ba9ad754fe8df9c1e833c9
Shyam Narayan Nayak <atsuya2728@gmail.com>
2024-11-06 12:11:14 +0530

    Add files via upload

---
+++ Day12-Number_guessing_game.py
@@ -0,0 +1,41 @@
+import random
+print("Number Guessing Game")
+print("Number is between 1 and 100")
+x= random.randrange(0,100)
+print(x)
+def guess():
+    global x
+    dif = input("Choose a difficulty : Easy or Hard ").lower()
+    if dif=="easy":
+        for i in range(-10,0):
+            print(f'You have {abs(i)} attempts remaining to guess')
+            tryy=int(input("Make a guess "))
+            if tryy<x:
+                print("too low")
+            elif tryy>x:
+                print("Too high")
+            else:
+                print("You won")
+                break
+        if tryy==x:
+            pass
+        else:
+            print("You ran out of guesses,You lose")
+
+    else:
+        for i in range(-5,0):
+            print(f'You have {abs(i)} attempts remaining to guess ')
+            tryy=int(input("Make a guess "))
+            if tryy<x:
+                print("too low")
+            elif tryy>x:
+                print("Too high")
+            else:
+                print("You won")
+                break
+        if tryy==x:
+            pass
+        else:
+            print("You ran out of guesses,You lose")
+
+guess()
